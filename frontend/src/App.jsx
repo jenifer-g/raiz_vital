b2744bd8f1ee1ca662786fda186856e9a4ba1eb6
Jennifer <22735@virtual.utsc.edu.mx>
2026-02-08 16:49:14 -0600

    inicio

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+import Navbar from './components/layout/Navbar';
+import Hero from './components/layout/Hero';
+function App() {
+  return (
+    <div className="min-h-screen bg-arena">
+      <Navbar />
+      <Hero></Hero>
+      <div>HOLA</div>
+    </div>
+  );
+}
+export default App
---
+++ frontend/src/components/layout/Hero.jsx
@@ -0,0 +1,24 @@
+import heroBg from '../../assets/hero.jpg';
+
+const Hero = () => {
+  return (
+    <section className="relative h-screen flex items-center justify-center text-white">
+      
+      <div 
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat" 
+        style={{ backgroundImage: `url(${heroBg})` }}
+      ></div>
+      
+      <div className="absolute inset-0 bg-black opacity-40"></div> 
+
+      <div className="relative z-10 text-center max-w-4xl w-full px-4">
+        <h1 className="text-5xl md:text-7xl font-bold mb-6 leading-tight">
+          Donde cada acción <br />
+          <span className="italic">echa raíces</span>
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
